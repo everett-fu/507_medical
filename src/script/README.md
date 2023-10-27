bb9f494113d97bedfe2c77cc401edfbaa2cf5751
flf <fulifan@126.com>
2023-10-27 16:06:39 +0800

    feat: 添加了自动化脚本，用于自动的爬取公交站台 docs: 修改了README.md文件

--- src/script/README.md
+++ src/script/README.md
@@ -6,19 +6,32 @@
 
 ---
 
+这个脚本一共有两个代码文件，分别是 `script.py` 与 `` 。
+`script.py` 为爬虫代码，用于爬取所有的公交车站台，需要修改代码中的 `城市名` 与`爬取类型` 。
+爬取完会将每个公交车站台的名称保存到 `data/` 目录下，并命名为 `城市名.txt`
+
+<br>
+
+`script1.py` 为获取坐标的代码，用于搜索出所有的公交车站台的坐标。需要读取 `城市名.txt` 文件。
+同时需要修改城市名，还需要高德地图的key，注意修改高德地图的key，否则无法使用。
+并且高德的key每个人每天只有5000次的免费使用次数，所以请注意。
+查询完以后，会将每个公交车站台的名称保存到 `data/` 目录下，命名为 `城市名.xls`。
+
 # 1. 代码说明
+
 
 ## 1.1. 爬取标签说明
 
 为了防止时间过长，导致界面大变样，在这边贴出了所有的界面元素，以便于后续的修改。
 
 并且防止失效，我将原代码拷贝到了 `src/script/` 目录下，以下为原来的网页代码：
-[源代码](./常熟公交查询_常熟公交车线路查询_常熟公交地图%20-%20常熟公交网.html)
+[界面一源代码](./常熟公交查询_常熟公交车线路查询_常熟公交地图%20-%20常熟公交网.html)
 
-原界面如下：
-![](images/main.png)
+### 1.1.1. 界面一
+界面一如下：
+![](images/main1.png)
 
-### 1.1.1. bus-layer depth w120
+#### 1.1.1.1 bus-layer depth w120
 代码中找到 `div` 标签的类名为 `bus-layer depth w120` 的元素，即下面的语句:
 ```
 soup_buslayer = soup.find('div', class_='bus-layer depth w120')
@@ -27,7 +40,7 @@ soup_buslayer = soup.find('div', class_='bus-layer depth w120')
 主要是找到以下的元素：
 ![](images/bus-layer%20depth.png)
 
-### 1.1.2. pl10
+#### 1.1.1.2. pl10
 
 代码中找到 `div` 标签的类名为 `pl10` 的元素，即下面的语句:
 ```
@@ -37,7 +50,7 @@ soup_buslist = soup_buslayer.find_all('div', class_='pl10')
 主要是找到以下的元素：
 ![](images/pl10.png)
 
-### 1.1.3. kt
+#### 1.1.1.3. kt
 代码中获取所有的分类名称，即下面的语句:
 ```
 name = soup_bus.find('span', class_='kt').get_text()
@@ -46,7 +59,7 @@ name = soup_bus.find('span', class_='kt').get_text()
 主要是找到以下的元素：
 ![](images/kt.png)
 
-### 1.1.4. list
+#### 1.1.1.4. list
 
 找到 `div` 标签的类名为 `list` 的元素，即下面的语句:
 ```
@@ -55,6 +68,52 @@ soup_a_list = soup_bus.find('div', class_='list')
 主要是找到以下的元素：
 ![](images/list.png)
 
-### 1.1.5. bus_i_t1
+##### 1.1.1.4.1 a
 
 `a`标签即上面的1~9
+
+### 1.1.2. 界面二
+
+当我们找到所有的以数字开头的公交车线路的地址以后，我们进入界面二。
+[界面二源代码](常熟1字头公交车_常熟1字头公交线路%20-%20常熟公交查询.html)
+界面二：
+![](images/main2.png)
+
+#### 1.1.2.1. list clearfix
+
+找到 `div` 标签的类名为 `list` 的元素，即下面的语句:
+```
+soup_buslist = soup.find('div', class_='list clearfix')
+```
+主要是找到以下的元素：
+![](images/list%20clearfix)
+
+##### 1.1.2.1.1. a
+
+`a`标签即上面的每一路公交车
+
+### 1.1.3. 界面三
+
+当我们选择某一路公交车以后，进入界面三，我们就可以抓取每个公交车的站台信息了。
+[界面三源代码](常熟1路_常熟1路公交车路线_常熟1路公交车路线查询_常熟1路公交车路线图.html)
+![](images/main3.png)
+
+#### 1.1.3.1. bus-lzlist mb15
+
+找到 `div` 标签的类名为 `bus-lzlist mb15` 的元素，即下面的语句:
+```
+soup_buslayer1 = soup.find('div', class_='bus-lzlist mb15')
+```
+
+主要是找到以下的元素:
+![](images/bus-lzlist%20mb15.png)
+
+##### 1.1.3.1.1. ol 标签
+
+![](images/ol.png)
+
+###### 1.1.3.1.1.1 li标签
+
+![](images/li.png)
+
++ `a` 标签，即为上面的站台名
--- src/script/script.py
+++ src/script/script.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-import json
-import xlwt
-# from Coordin_transformlat import gcj02towgs84
 
 '''
 我们使用的是 `https://8684.cn/ `这个网站
 我们是以常熟为例，爬取常熟的公交线路数据，需要修改以下地方：
 常熟市的公交数据: https://changshu.8684.cn/
 
+注意，我们的是不删除文件的，会直接在文件后面添加，如果没有删除文件，直接运行程序，会导致数据一直添加，会出现重复的数据
+
 请注意，网站很有可能会更新，所以如果有界面变动，请修改标签的类名
 
+
+本段代码需要修改以下的代码:
 1. city = '' # 此处修改城市
 2. type = '' # 此处修改需要爬取的公交线路的类型
 
@@ -18,15 +19,20 @@ import xlwt
 
 # 请确保你的城市名是对的，建议先在网站上搜索一下，看看网站上的城市名是什么
 city = 'changshu' # 此处修改城市
-url = 'https://' + city + '.8684.cn/'
+url = 'https://' + city + '.8684.cn'
 
 # 这里按照你的需求修改，我们是爬取 `以数字开头` 的公交路线
 type = '以数字开头'
 
+
+# 进入界面一
+
+
 # 发起 HTTP 请求，获取城市数据
 response = requests.get(url=url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'})
 # 打印请求的状态码，如果返回200，则证明请求成功
-print(response. status_code)
+if (response.status_code == 200):
+    print("连接成功")
 
 #获取数据并解析
 soup = BeautifulSoup(response.text, 'lxml')
@@ -54,7 +60,7 @@ for soup_bus in soup_buslist:
             # 获取链接地址
             href = soup_a.get('href')
             # 将线路名称和 URL 存储到字典中
-            dic_result[text] = city + href #此处需要修改
+            dic_result[text] = url + href #此处需要修改
 
             
 #
@@ -62,3 +68,104 @@ for soup_bus in soup_buslist:
 # print(dic_result)
 #
 #
+
+
+# 进入界面二
+
+
+#获取公交站点地址        
+# 存储公交站点地址的列表
+bus_arr = []
+# 记录当前遍历到的公交车站点的索引
+index = 0
+# 
+start_num = 0
+bus_arr_start_num = 0
+
+# 遍历公交站点列表
+for key,value in dic_result.items():
+    # 发送 HTTP 请求，获取公交线路
+    response = requests.get(url=value, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'})
+    #获取数据并解析
+    soup = BeautifulSoup(response.text, 'lxml')
+    # 详细线路
+    # 找到 `div` 标签的类名为 `list clearfix` 的元素
+    soup_buslist = soup.find('div', class_='list clearfix')
+    # 判断是否为空，因为有的城市没有以数字开头的公交车
+    if soup_buslist != None:
+        # 找到 `list clearfix` 标签名为 `a` 的元素
+        soup_list2=soup_buslist.find_all('a')
+        # 遍历当前界面所有的公交车路线
+        for soup_a in soup_list2:
+            # 获取 `a` 标签的名称
+            text = soup_a.get_text()
+            # 获取 `a` 标签的链接
+            href = soup_a.get('href')
+            # 获取 `a` 标签的title
+            title = soup_a.get('title')
+            # 储存到列表中
+            bus_arr.append([title, text, url + href])
+            
+            
+        #
+        # 测试语句，判断上面的代码是否正确
+        # print(bus_arr)
+        #
+        #
+
+
+    # 进入界面三
+
+
+    # 创建一个空集合来存储公交站点名称
+    s = set()
+
+    # 打开文件
+    with open("data/" + city + '.txt', 'ab') as f:
+        # 指向文件最后，即在文件最后插入
+        f.seek(0, 2)  # set file pointer to end of file
+
+        # 遍历公交站点的 URL
+        for i in range(bus_arr_start_num, len(bus_arr)):
+            bus_arr_start_num = len(bus_arr)
+            # 输出进度条
+            rate = int(float(i) / len(bus_arr) * 100)
+            print("\r进度: {}% [{}{}]".format(rate, '*' * int(rate / 10), ' ' * (10 - int(rate / 10))), end="")
+
+            # 获取公交车站点的 URL
+            url_busstop = bus_arr[i][2]
+            # 将公交车的编码设置成 `utf-8`
+            key_str = bus_arr[i][1].encode("utf-8")
+            # 发送 HTTP 请求，获得公交车的具体站点
+            response = requests.get(url=url_busstop, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'})
+            # 获取数据并解析
+            soup = BeautifulSoup(response.text, 'lxml')
+            # 找到 `div` 标签的类名为 `bus-lzlist mb15` 的元素
+            soup_buslayer1 = soup.find('div', class_='bus-lzlist mb15')
+            # 判断有没有这个标签
+            if soup_buslayer1 != None:
+                # 读取里面的所有ol
+                for a in soup_buslayer1.find_all('ol'):
+                    # 读取里面的所有li
+                    for busstop in a.find_all('li'):
+                        # 读取里面的所有 `a` 标签
+                        busstopfinal = busstop.find('a')
+                        # 判断是否有这个标签
+                        if busstopfinal != None:
+                            # 获取站台名
+                            text = busstopfinal.get('aria-label')
+                            # 判断是否为空
+                            if text != None:
+                                # 设置站台名编码为 utf-8
+                                str = text.split()[1].encode("utf-8")
+                                if str not in s:
+                                    s.add(str)
+                                    # 将公交线路信心写入文件
+                                    f.write(key_str + b' ' + str + b'\n')
+                                    start_num += 1
+    f.close()
+    print("以数字" + key + "开头的公交车数据爬取完成\n")
+print("所有的数据爬取完成")
+
+
