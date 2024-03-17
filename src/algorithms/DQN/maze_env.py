5b164550ee3cef9025137caaf2fbdb0cd1f42ebf
everett-fu <everett.fu@gmail.com>
2024-03-17 16:07:48 +0800

    完成maze_env.py的step代码

--- src/algorithms/DQN/maze_env.py
+++ src/algorithms/DQN/maze_env.py
@@ -13,15 +13,17 @@ class EV:
         self.number_nodes = number_nodes
         self.number_charge = number_charge
         # 初始化地图
+        # 这个代码是从已有的地图中选一个点作为起始点，所以动作数量是地图点位
         rows = self.number_nodes
         self.action_space = []  # 可选路段集合
+        # 这个代码是从已有的地图以外选一个点作为起始点，所以动作数量是地图点位加上电车起始点的数量
         # rows = self.number_nodes + self.number_evs + 1
         cols = rows
         for i in range(rows):
             for j in range(cols):
                 self.action_space.append((i, j))
         self.n_actions = len(self.action_space)
-        self.n_features = 3
+        self.n_features = 4
         print(self.n_actions, self.n_features, self.action_space)
 
     # 初始化交通地图
@@ -49,46 +51,61 @@ class EV:
         return [segment_0, segment_1, E_0, t_0]
 
     # 执行动作
-    def step(self, starting, observation, action_space, action, graph_speed, graph, ending, path_id):
+    # 输入电车起始点，当前状态，动作空间，动作，距离矩阵，充电矩阵，速度矩阵，走过路径的编号
+    def step(self, starting, observation, action_space, action, graph, graph_charge, graph_speed, path_id):
         # 当前状态：当前路段，目标地址，剩余电量，剩余deadline
         segment1, segment2, E1, t1 = observation[0], observation[1], observation[2], observation[3]
-        s_ = [0, 0, 0, 0]
-        done = False
-        start = action_space[segment1][0]   # 当前路段的起点
-        end = action_space[segment1][1]     # 当前路段的终点
-        r_e_a = graph_speed[start][end]     # EV行驶速度km/h
-        r_b = 40    # speed of bus 40km/h
-        charging_power = 100    # 公共巴士充电功率100kw
-        E_e_max = 100   # 电车的电池容量kWh
-        gamma = 1   # 电车的能耗，每公里消耗1kWh
-        reward = -gamma * graph[start][end]     # 走完该路段的能耗
-        E_ = E1 - gamma * graph[start][end]     # 剩余电量
-        if r_e_a == 0:
+        s_ = [0, 0, 0, 0]                       # 下一状态
+        done = False                            # 是否结束
+        start = action_space[segment1][0]       # 当前路段的起点，即从A出发到B的A
+        end = action_space[segment1][1]         # 当前路段的终点，即从A出发到B的B
+        speed = graph_speed[start][end]         # 当前路段的行驶速度
+        gamma = 1                               # 能耗，每公里消耗1kWh
+        charge_power = 100                      # 充电功率，每小时100kWh
+
+        charge = 0                              # 电量变化值
+        # 如果当前路段是充电路段，加上充电量减去耗电量
+        if graph_charge[start][end] == 1:
+            charge += charge_power * graph[start][end] / speed - graph[start][end] * gamma
+        else:
+            charge += -graph[start][end] * gamma
+        # 更新剩余电量
+        E_ = E1 + charge
+        # 电量最大为100
+        if E_ > 100:
+            E_ = 100
+
+        # 更新奖励函数
+        reward = charge
+
+        # 更新剩余时间
+        if speed == 0:
             t_ = -1000
         else:
-            t_ = t1 - graph[start][end] / r_e_a
+            t_ = t1 - graph[start][end] / speed
+
+        # 提取出访问过的点级
         visited_path = []
         for idd in path_id:
             visited_path.append(action_space[idd])
-        visited_path = np.array(visited_path).flatten()  # 二维列表转换为一维列表
-        vp = set(visited_path)  # 路径访问过的点集合
-        if end == ending:  # 到达终点
-            s_ = [action, E_, t_]
-            print("[ev_env.step]终止调度，到达终点了，s_=", s_)
-            done = True  # 终止调度
+        visited_path = np.array(visited_path).flatten()
+        vp = set(visited_path)
+
+        # 如果到达终点
+        if end == segment2:
+            s_ = [action, segment2, E_, t_]
+            done = True
         else:
-            if action_space[action][0] == end and (action_space[action][1] in vp) == False and (
-                    action_space[action][1] == starting) == False:
+            # 如果当前路段不是从终点出发，且不在访问过的点集合中，且没有绕回到起点
+            if action_space[action][0] == end and (action_space[action][1]) not in vp and (action_space[action][1] != starting):
+                # 如果电量为0或者时间为0，退出，失败
                 if E_ <= 0 or t_ <= 0:
-                    # 剩余电量为0或者deadline为0，退出，失败。
-                    print("[ev_env.step]不满足约束了,E_,t_=", E_, t_)
-                    reward = 0
-                    done = True  # 终止调度
-                else:  # 继续调度
-                    s_ = [action, E_, t_]
-                    print("[ev_env.step]继续调度s_=", s_)
-            else:  # 当前路段无法使用
+                    reward = -100
+                    done = True
+                else:
+                    s_ = [action, segment2, E_, t_]
+            else:
                 done = True
-                reward = 0
+                reward = -100
 
         return s_, reward, done
